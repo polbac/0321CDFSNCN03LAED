83dd5fecf8aaa2dd3cf19a62b941646f79a9de97
gonzalozevallos <gonzalo.zev@gmail.com>
2021-07-09 17:54:01 -0300

    c32-manipulación de datos: preclase

---
+++ m6-db/c32-Manipulación de datos/live-coding/src/database/models/User.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataType) => {
+    const alias = 'User'
+    /* camelCase por default */
+    const cols = {  
+        name: {
+            type: DataType.STRING
+        },
+        email: {
+            type: DataType.INTEGER,
+        },
+        password: {
+            type: DataType.BOOLEAN
+        },
+        image: {
+            type: DataType.STRING
+        },
+    }
+    
+    const config = {
+        underscored: true, /* te busca en snake case las columnas */
+        timestamps: false /* no va a buscar las columnas de timestamps */
+    }
+    
+    const UserModel = sequelize.define(alias, cols, config)
+
+    return UserModel
+}           
