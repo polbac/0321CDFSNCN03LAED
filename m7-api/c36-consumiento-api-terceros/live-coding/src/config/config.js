fc91ffc37f9f8087037a512974c467f025b95037
polbac <polbac@gmail.com>
2021-07-23 22:27:12 -0300

    c37

--- m7-api/c36-consumiento-api-terceros/live-coding/src/config/config.js
+++ m7-api/c36-consumiento-api-terceros/live-coding/src/config/config.js
@@ -2,4 +2,6 @@ module.exports = {
     sessionSecret: 'PLANETAS',
     cookiesSecret: 'COOKIES PLANETAS',
     maxAgeUserCookie: 1000 * 60 * 6,
+    nasaApiKey: 'ynvebr51zLjTnIL6ASXs4auV1bOmcA2IpISzCwzo',
+    endpointPhotosMars: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000',
 }
--- m7-api/c36-consumiento-api-terceros/live-coding/src/controllers/satelliteController.js
+++ m7-api/c36-consumiento-api-terceros/live-coding/src/controllers/satelliteController.js
@@ -1,18 +1,7 @@
+const { getPhotosFromMars } = require('../resources/nasa')
 module.exports = {
-    listPhotos(req, res) {
-        const photos = [
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-            { image: 'https://services.meteored.com/img/article/si-los-romanos-hubieran-lanzado-un-satelite-caeria-hoy-316651-1_1280.jpg', title: 'Titulo foto' },
-        ]
-
-        res.render('satellite/photos', { photos })
+    async listPhotos(req, res) {
+        const planetsFromMars = await getPhotosFromMars()
+        res.render('satellite/photos', { photos: planetsFromMars.photos })
     }
 }
